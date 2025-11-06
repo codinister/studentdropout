60222186ef0402295bd5cea0c90a445aca61d654
codinister <codinister@gmail.com>
2025-11-06 21:03:48 +0100

    Commit 54

--- app/api/students/get-students/route.ts
+++ app/api/students/get-students/route.ts
@@ -4,7 +4,7 @@ import { NextResponse } from 'next/server';
 export const dynamic = 'force-dynamic'
 export const revalidate = 0 
 
-export async function GET(){
+export async function GET(): Promise<any> {
   try {
     const student = await db.student.findMany();
     return NextResponse.json(student)
